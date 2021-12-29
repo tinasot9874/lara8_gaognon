48fc71c9c08b6c6a97c9931f31f94ae1ed2fca9a
Hieu Tran <tinasot9874@gmail.com>
2021-12-29 17:44:52 +0700

    init template

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,166 @@
+@extends('admin.layout')
+
+@section('content')
+<!-- page-wrapper Start-->
+<div class="page-wrapper">
+
+    <!-- Page Header Start-->
+    <div class="page-main-header">
+        <div class="main-header-right row">
+            <div class="main-header-left d-lg-none w-auto">
+                <div class="logo-wrapper"><a href="{{route('admin.dashboard')}}"><img class="blur-up lazyloaded" src="{{asset('images/dashboard/multikart-logo.png')}}" alt=""></a></div>
+            </div>
+            <div class="mobile-sidebar w-auto">
+                <div class="media-body text-end switch-sm">
+                    <label class="switch"><a href="#"><i id="sidebar-toggle" data-feather="align-left"></i></a></label>
+                </div>
+            </div>
+            <div class="nav-left col">
+                <div class="page-header-left">
+                    <ol class="breadcrumb pull-left">
+                        <li class="breadcrumb-item"><a href="{{route('admin.dashboard')}}"><i data-feather="home"></i></a></li>
+                        <li class="breadcrumb-item active">Dashboard</li>
+                    </ol>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    <!-- Page Header Ends -->
+
+    <!-- Page Body Start-->
+    <div class="page-body-wrapper">
+
+        <!-- Page Sidebar Start-->
+        <div class="page-sidebar">
+            <div class="main-header-left d-none d-lg-block">
+                <div class="logo-wrapper"><a href="{{route('admin.dashboard')}}"><img class="blur-up lazyloaded" src="{{asset('images/dashboard/multikart-logo.png')}}" alt=""></a></div>
+            </div>
+            <div class="sidebar custom-scrollbar">
+                <div class="sidebar-user text-center">
+                    <div><img class="img-60 rounded-circle lazyloaded blur-up" src="{{asset('images/dashboard/man.png')}}" alt="#">
+                    </div>
+                    <h6 class="mt-3 f-14">JOHN</h6>
+                    <p>general manager.</p>
+                </div>
+                <ul class="sidebar-menu">
+                    <li><a class="sidebar-header" href="{{route('admin.dashboard')}}"><i data-feather="home"></i><span>Dashboard</span></a></li>
+                    <li><a class="sidebar-header" href="#"><i data-feather="box"></i> <span>Products</span><i class="fa fa-angle-right pull-right"></i></a>
+                        <ul class="sidebar-submenu">
+                            <li>
+                                <a href="#"><i class="fa fa-circle"></i>
+                                    <span>Physical</span> <i class="fa fa-angle-right pull-right"></i>
+                                </a>
+                                <ul class="sidebar-submenu">
+                                    <li><a href="category.html"><i class="fa fa-circle"></i>Category</a></li>
+                                    <li><a href="category-sub.html"><i class="fa fa-circle"></i>Sub Category</a></li>
+                                    <li><a href="product-list.html"><i class="fa fa-circle"></i>Product List</a></li>
+                                    <li><a href="product-detail.html"><i class="fa fa-circle"></i>Product Detail</a></li>
+                                    <li><a href="add-product.html"><i class="fa fa-circle"></i>Add Product</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#"><i class="fa fa-circle"></i>
+                                    <span>Digital</span> <i class="fa fa-angle-right pull-right"></i>
+                                </a>
+                                <ul class="sidebar-submenu">
+                                    <li><a href="category-digital.html"><i class="fa fa-circle"></i>Category</a></li>
+                                    <li><a href="category-digitalsub.html"><i class="fa fa-circle"></i>Sub Category</a></li>
+                                    <li><a href="product-listdigital.html"><i class="fa fa-circle"></i>Product List</a></li>
+                                    <li><a href="add-digital-product.html"><i class="fa fa-circle"></i>Add Product</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                    <li><a class="sidebar-header" href=""><i data-feather="dollar-sign"></i><span>Sales</span><i class="fa fa-angle-right pull-right"></i></a>
+                        <ul class="sidebar-submenu">
+                            <li><a href="order.html"><i class="fa fa-circle"></i>Orders</a></li>
+                            <li><a href="transactions.html"><i class="fa fa-circle"></i>Transactions</a></li>
+                        </ul>
+                    </li>
+                    <li><a class="sidebar-header" href=""><i data-feather="tag"></i><span>Coupons</span><i class="fa fa-angle-right pull-right"></i></a>
+                        <ul class="sidebar-submenu">
+                            <li><a href="coupon-list.html"><i class="fa fa-circle"></i>List Coupons</a></li>
+                            <li><a href="coupon-create.html"><i class="fa fa-circle"></i>Create Coupons </a></li>
+                        </ul>
+                    </li>
+                    <li><a class="sidebar-header" href="#"><i data-feather="clipboard"></i><span>Pages</span><i class="fa fa-angle-right pull-right"></i></a>
+                        <ul class="sidebar-submenu">
+                            <li><a href="pages-list.html"><i class="fa fa-circle"></i>List Page</a></li>
+                            <li><a href="page-create.html"><i class="fa fa-circle"></i>Create Page</a></li>
+                        </ul>
+                    </li>
+                    <li><a class="sidebar-header" href="media.html"><i data-feather="camera"></i><span>Media</span></a></li>
+                    <li><a class="sidebar-header" href="#"><i data-feather="align-left"></i><span>Menus</span><i class="fa fa-angle-right pull-right"></i></a>
+                        <ul class="sidebar-submenu">
+                            <li><a href="menu-list.html"><i class="fa fa-circle"></i>Menu Lists</a></li>
+                            <li><a href="create-menu.html"><i class="fa fa-circle"></i>Create Menu</a></li>
+                        </ul>
+                    </li>
+                    <li><a class="sidebar-header" href=""><i data-feather="user-plus"></i><span>Users</span><i class="fa fa-angle-right pull-right"></i></a>
+                        <ul class="sidebar-submenu">
+                            <li><a href="user-list.html"><i class="fa fa-circle"></i>User List</a></li>
+                            <li><a href="create-user.html"><i class="fa fa-circle"></i>Create User</a></li>
+                        </ul>
+                    </li>
+                    <li><a class="sidebar-header" href=""><i data-feather="users"></i><span>Vendors</span><i class="fa fa-angle-right pull-right"></i></a>
+                        <ul class="sidebar-submenu">
+                            <li><a href="list-vendor.html"><i class="fa fa-circle"></i>Vendor List</a></li>
+                            <li><a href="create-vendors.html"><i class="fa fa-circle"></i>Create Vendor</a></li>
+                        </ul>
+                    </li>
+                    <li><a class="sidebar-header" href=""><i data-feather="chrome"></i><span>Localization</span><i class="fa fa-angle-right pull-right"></i></a>
+                        <ul class="sidebar-submenu">
+                            <li><a href="translations.html"><i class="fa fa-circle"></i>Translations</a></li>
+                            <li><a href="currency-rates.html"><i class="fa fa-circle"></i>Currency Rates</a></li>
+                            <li><a href="taxes.html"><i class="fa fa-circle"></i>Taxes</a></li>
+                        </ul>
+                    </li>
+                    <li><a class="sidebar-header" href="reports.html"><i data-feather="bar-chart"></i><span>Reports</span></a></li>
+                    <li><a class="sidebar-header" href=""><i data-feather="settings" ></i><span>Settings</span><i class="fa fa-angle-right pull-right"></i></a>
+                        <ul class="sidebar-submenu">
+                            <li><a href="profile.html"><i class="fa fa-circle"></i>Profile</a></li>
+                        </ul>
+                    </li>
+                    <li><a class="sidebar-header" href="invoice.html"><i data-feather="archive"></i><span>Invoice</span></a>
+                    </li>
+                    <li><a class="sidebar-header" href="login.html"><i data-feather="log-in"></i><span>Login</span></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <!-- Page Sidebar Ends-->
+
+
+        <div class="page-body">
+
+            <!-- Container-fluid starts-->
+            <div class="container-fluid">
+
+
+
+            </div>
+            <!-- Container-fluid Ends-->
+
+
+
+        </div>
+
+        <!-- footer start-->
+        <footer class="footer">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-6 footer-copyright">
+                        <p class="mb-0">Copyright 2019 © Multikart All rights reserved.</p>
+                    </div>
+                    <div class="col-md-6">
+                        <p class="pull-right mb-0">Hand crafted & made with<i class="fa fa-heart"></i></p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- footer end-->
+    </div>
+
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::name('admin.')->prefix('admin')->group(function () {
+    Route::get('/', function () {
+
+        return view('admin.dashboard');
+
+    })->name('dashboard');
 });
+
+
